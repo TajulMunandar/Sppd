b38605776ff4ad1f21606794b076b222e95ed0f7
mukhlizar16 <mukhlizaracer@gmail.com>
2025-03-09 23:53:22 +0700

    feat: riwayat surat tugas

--- app/Http/Controllers/RiwayatSuratTugasController.php
+++ app/Http/Controllers/RiwayatSuratTugasController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class RiwayatSuratTugasController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
         $title = 'Riwayat Surat Tugas';
         return view('admin.surat_tugas.riwayat.index', compact('title'));
